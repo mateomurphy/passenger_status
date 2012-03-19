2a2e7aa75f7c58e87a3a15471f1357c473d59382
Mateo Murphy <mateo.murphy@gmail.com>
2012-03-19 15:29:42 -0400

    Use cocaine for running passenger-status command

--- lib/passenger_status/system.rb
+++ lib/passenger_status/system.rb
@@ -1,10 +1,17 @@
+require 'cocaine'
+
 module PassengerStatus
   class System
     def data
-      `passenger-status --show=xml`
+      @data ||= data_command.run
+    end
+    
+    def data_command
+      @data_command ||= Cocaine::CommandLine.new('passenger-status', '--show=xml', :swallow_stderr => true)
     end
     
     def kill(pid)
+      PassengerStatus.logger.info "Killing process with pid #{pid}"
       `kill -9 #{pid}`
     end
   end
